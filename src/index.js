3377f5354635b8c5d41460f1c7c228528af887da
Wouter Landuydt <wouter.landuydt@gmail.com>
2018-09-11 13:38:46 +0200

    :lipstick: init styled-components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
+import { injectGlobal } from "styled-components";
+import reset from "styled-reset";
 import App from "./containers/App";
 import configureStore from "./store/configureStore";
 import rootSaga from "./sagas";
 
 const store = configureStore();
 store.runSaga(rootSaga);
+
+injectGlobal`
+  ${reset}
+`;
 
 render(
   <Provider store={store}>
